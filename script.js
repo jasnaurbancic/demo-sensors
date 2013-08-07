0230c60ecb867c577a8a19cbf832c607467fc530
jasnaurbancic <jasna.urbancic@zemanta.com>
2013-08-07 19:25:56 +0200

    First idea

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  if (window.DeviceMonitorEvent) {
+    window.addEventListener('devicemotion', deviceMotionHandler, false);
+  } else {
+    $('div').after("<p>Not supported</p>");
+  }
+  function deviceMotionHandler(eventData){
+    var acceleration = eventData.acceleration;
+    if (acceleration.x > 0.0){
+      $('div').animate({left: "+=10%"}, 400);
+    }
+    if (acceleration.x < 0.0){
+      $('div').animate({left: "-=10%"}, 400);
+    }
+    if (acceleration.y > 0.0){
+      $('div').animate({top: "-=10%"}, 400);
+    }
+    if (acceleration.y < 0.0){
+      $('div').animate({left: "+=10%"}, 400);
+    }
+    if (acceleration.z > 0.0){
+      $('div').animate({height: "+=10%", width: "+=10%"}, 400);
+    }
+    if (acceleration.z < 0.0){
+      $('div').animate({height: "-=10%", width: "-=10%"}, 400);
+    }
+  };
+}); 
