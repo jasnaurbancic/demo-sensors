105e360c4ffa567e79f0871e79f79a3c60412ca4
jasnaurbancic <jasna.urbancic@zemanta.com>
2013-08-08 10:17:22 +0200

    Debug

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-  if (window.DeviceMonitorEvent) {
+ if (window.DeviceMonitorEvent) {
     window.addEventListener('devicemotion', deviceMotionHandler, false);
   } else {
     $('div').after("<p>Not supported</p>");
   }
   function deviceMotionHandler(eventData){
     var acceleration = eventData.acceleration;
-    if (acceleration.x > 0.0){
+    $('div').after("<p>Acceleration detected!</p>");
+    /*if (acceleration.x > 0.0){
       $('div').animate({left: "+=10px"}, 400);
     }
     if (acceleration.x < 0.0){
@@ -23,6 +24,6 @@ $(document).ready(function(){
     }
     if (acceleration.z < 0.0){
       $('div').animate({height: "-=10px", width: "-=10px"}, 400);
-    }
+    }*/
   };
 }); 
